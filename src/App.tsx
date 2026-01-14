9828b3ad2f9e9dd41b07b8f3369c553fa3a514c2
friranger <65720979+friranger1@users.noreply.github.com>
2026-01-14 20:45:41 +0200

    Add message with button been pressed last in a React class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-export const App: React.FC = () => {
-  const [lastPressedKey, setLastPressedKey] = useState<string>('');
-
-  useEffect(() => {
-    const keypButton = (event: KeyboardEvent) => {
-      setLastPressedKey(event.key);
-    };
-
-    document.addEventListener('keyup', keypButton);
-
-    return () => {
-      document.removeEventListener('keyup', keypButton)
-    }
-  }, []);
-
-  return (
-    <div className="App">
-      <p className="App__message">{lastPressedKey === '' ? `Nothing was pressed yet` : `The last pressed key is [${lastPressedKey}]`}</p>
-    </div>
-  )
+type State = {
+  lastPressedKey: string;
 };
+
+type Props = {};
+
+export class App extends React.Component<Props, State> {
+  state: State = {
+    lastPressedKey: '',
+  };
+
+  keyUpButton = (event: KeyboardEvent) => {
+    this.setState({
+      lastPressedKey: event.key,
+    });
+  };
+
+  componentDidMount(): void {
+    window.addEventListener('keyup', this.keyUpButton);
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener('keyup', this.keyUpButton);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.lastPressedKey === ''
+            ? `Nothing was pressed yet`
+            : `The last pressed key is [${this.state.lastPressedKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
